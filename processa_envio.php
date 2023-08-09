7e7fa0461942e2f33b26d6d11b7d24b8329c57cb
wevdiaz <63312710+wevdiaz@users.noreply.github.com>
2023-08-09 19:59:58 -0300

    add library PHPMailer

--- processa_envio.php
+++ processa_envio.php
@@ -1,7 +1,12 @@
 <?php 
-    echo '<pre>';
-    print_r($_POST);
-    echo '</pre>';
+    require "./bibliotecas/PHPMailer/Exception.php";
+    require "./bibliotecas/PHPMailer/OAuth.php";
+    require "./bibliotecas/PHPMailer/PHPMailer.php";
+    require "./bibliotecas/PHPMailer/POP3.php";
+    require "./bibliotecas/PHPMailer/SMTP.php";
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
 
     class Mensagem {
       private $destinatario = null;
@@ -31,10 +36,45 @@
     $mensagem->__set('assunto', $_POST['assunto']);
     $mensagem->__set('mensagem', $_POST['mensagem']);
 
-    if($mensagem->mensagemValida()) {
-      echo '<span style="color:green">Mensagem Válida</span>';
-    } else {
-      echo '<span style="color:red">Mensagem Inválida</span>';
+    if(!$mensagem->mensagemValida()) {
+      echo '<span style="color:red">Mensagem Inválida</span>';      
+    } 
+
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = false;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'user@example.com';                     //SMTP username
+        $mail->Password   = 'secret';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        //Recipients
+        $mail->setFrom('from@example.com', 'Mailer');
+        $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+        $mail->addAddress('ellen@example.com');               //Name is optional
+        $mail->addReplyTo('info@example.com', 'Information');
+        $mail->addCC('cc@example.com');
+        $mail->addBCC('bcc@example.com');
+
+        //Attachments
+        $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Não foi possível enviar sua mensagem por email. Mailer Error: {$mail->ErrorInfo}";
     }
 
 ?>
