5860e5c71c4c6a5c0cd5759abf14703e96200326
wevdiaz <63312710+wevdiaz@users.noreply.github.com>
2023-08-10 16:17:45 -0300

    settings PHPMailer and sending email

--- processa_envio.php
+++ processa_envio.php
@@ -1,8 +1,8 @@
 <?php 
     require "./bibliotecas/PHPMailer/Exception.php";
-    require "./bibliotecas/PHPMailer/OAuth.php";
+    // require "./bibliotecas/PHPMailer/OAuth.php";
     require "./bibliotecas/PHPMailer/PHPMailer.php";
-    require "./bibliotecas/PHPMailer/POP3.php";
+    // require "./bibliotecas/PHPMailer/POP3.php";
     require "./bibliotecas/PHPMailer/SMTP.php";
 
     use PHPMailer\PHPMailer\PHPMailer;
@@ -46,30 +46,30 @@
         //Server settings
         $mail->SMTPDebug = false;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'user@example.com';                     //SMTP username
-        $mail->Password   = 'secret';                               //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        $mail->Username   = 'userwzteste@gmail.com';                     //SMTP username
+        $mail->Password   = 'wrohqurjcvkwfnen';                               //SMTP password
+        $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
-        $mail->setFrom('from@example.com', 'Mailer');
-        $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-        $mail->addAddress('ellen@example.com');               //Name is optional
-        $mail->addReplyTo('info@example.com', 'Information');
-        $mail->addCC('cc@example.com');
-        $mail->addBCC('bcc@example.com');
+        $mail->setFrom('userwzteste@gmail.com', 'Usuário Remetente');
+        $mail->addAddress('userwzteste@gmail.com', 'Usuário Destinatário');     //Add a recipient
+        // $mail->addAddress('ellen@example.com');               //Name is optional
+        // $mail->addReplyTo('info@example.com', 'Information');
+        // $mail->addCC('cc@example.com');
+        // $mail->addBCC('bcc@example.com');
 
         //Attachments
-        $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-        $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+        // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        $mail->Subject = 'Assunto Promocional';
+        $mail->Body    = 'Esse é o conteúdo da promoção enviada por <strong>email</strong>';
+        $mail->AltBody = 'Esse é o conteúdo da promoção enviada por email';
 
         $mail->send();
         echo 'Message has been sent';
